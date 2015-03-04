11cb5d9dcf0d1c920681e2f0901414f7eb3f3da0
jeanbelhache <jeanbelhache@gmail.com>
2015-03-04 12:53:50 +0100

    doctrine2l5

--- src/config/d2cache.php
+++ src/config/d2cache.php
@@ -2,10 +2,10 @@
 
 /*
 |--------------------------------------------------------------------------
-| Doctrine2Bridge :: Cache Configuration
+| Doctrine2j5 :: Cache Configuration
 |--------------------------------------------------------------------------
 |
-| See: https://github.com/opensolutions/doctrine2bridge-l5
+| See: https://github.com/jeanbelhache/doctrine2-l5
 |
 | Caching configuration for Doctrine2. Uses Laravel's own cache configuration
 | from config/cache.php to create a Dcotrine2 cache object.
@@ -16,7 +16,7 @@
 |
 | Any cache such as ArrayCache requiring no configuration can be named below.
 | Caches requiring configuration will require updating:
-|     src/Doctrine2l5/Doctrine2CacheBridgeServiceProvider.php
+|     src/Doctrine2l5/Doctrine2CacheServiceProvider.php
 |
 | It should be fairly trivial to do this. Please open a pull request when you do!
 |
